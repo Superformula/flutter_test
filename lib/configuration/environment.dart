5af47cd9853c1a0ca7d7dfa5fd8193ba949e0185
Bruno Giacobo Pinto <bruno.giacobo.pinto@gmail.com>
2024-09-07 20:32:57 -0300

    feat(app): improve security by loading sensitive data from dart-defines

---
+++ lib/configuration/environment.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+
+// We should not expose any sensitive information in the code.
+// Thus, we pass the baseUrl as well as the API Key as dart-defines.
+// These values will come from the CI environment, which is a trustwhorty environement.
+final class Environment {
+  static const baseUrl = String.fromEnvironment('API_URL');
+  static const apiKey = String.fromEnvironment('API_KEY');
+  static const kDefaultTimeout = Duration(seconds: 15);
+
+  static Dio createDioClient({String? url, String? key, Duration? cadidateTimeout}) {
+    final baseUrl = url ?? Environment.baseUrl;
+    final apiKey = key ?? Environment.apiKey;
+    final timeout = cadidateTimeout ?? kDefaultTimeout;
+
+    return Dio(
+      BaseOptions(
+        baseUrl: baseUrl,
+        headers: {
+          'Authorization': 'Bearer $apiKey',
+          'Content-Type': 'application/graphql',
+        },
+        sendTimeout: timeout,
+        connectTimeout: timeout,
+        receiveTimeout: timeout,
+      ),
+    );
+  }
+}
