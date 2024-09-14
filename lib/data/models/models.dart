852e66e13f34b0a668ba269f5ca92351db99b2fa
Alex Oliveira <allex.c.o@hotmail.com>
2024-09-14 20:45:12 -0300

    feat: add restaurant model

--- lib/data/models/models.dart
+++ lib/data/models/models.dart
@@ -1,3 +1,4 @@
 export 'category_model.dart';
+export 'restaurant_model.dart';
 export 'review_model.dart';
 export 'user_model.dart';
---
+++ lib/data/models/restaurant_model.dart
@@ -0,0 +1,83 @@
+import '../../domain/entities/entities.dart';
+import '../../domain/helpers/helpers.dart';
+import 'models.dart';
+
+class RestaurantModel {
+  final String id;
+  final List<CategoryModel> categories;
+  final String displayCategory;
+  final String heroImage;
+  final bool isOpen;
+  final String address;
+  final String name;
+  final List<String> photos;
+  final String price;
+  final int rating;
+  final List<ReviewModel> reviews;
+
+  RestaurantModel({
+    required this.id,
+    required this.categories,
+    required this.displayCategory,
+    required this.heroImage,
+    required this.isOpen,
+    required this.address,
+    required this.name,
+    required this.photos,
+    required this.price,
+    required this.rating,
+    required this.reviews,
+  });
+
+  factory RestaurantModel.fromEntity(RestaurantEntity entity) {
+    return RestaurantModel(
+      id: entity.id,
+      rating: entity.rating,
+      categories: entity.categories.map((category) => CategoryModel.fromEntity(category)).toList(),
+      displayCategory: entity.displayCategory,
+      heroImage: entity.heroImage,
+      isOpen: entity.isOpen,
+      address: entity.address,
+      name: entity.name,
+      photos: entity.photos,
+      price: entity.price,
+      reviews: entity.reviews.map((review) => ReviewModel.fromEntity(review)).toList(),
+    );
+  }
+
+  factory RestaurantModel.fromJson(Map json) {
+    try {
+      return RestaurantModel(
+        id: json['id'],
+        categories: (json['categories'] as List).map((item) => CategoryModel.fromJson(json)).toList(),
+        displayCategory: json['displayCategory'],
+        heroImage: json['heroImage'],
+        isOpen: json['isOpen'],
+        address: json['address'],
+        name: json['name'],
+        photos: json['photos'],
+        price: json['price'],
+        rating: json['rating'],
+        reviews: (json['reviews'] as List).map(((item) => ReviewModel.fromJson(item))).toList(),
+      );
+    } catch (_) {
+      throw DomainError.unexpected;
+    }
+  }
+
+  RestaurantEntity toEntity() {
+    return RestaurantEntity(
+      id: id,
+      rating: rating,
+      categories: categories.map((category) => category.toEntity()).toList(),
+      displayCategory: displayCategory,
+      heroImage: heroImage,
+      isOpen: isOpen,
+      address: address,
+      name: name,
+      photos: photos,
+      price: price,
+      reviews: reviews.map((review) => review.toEntity()).toList(),
+    );
+  }
+}
