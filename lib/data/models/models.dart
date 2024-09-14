081d94434df81aa86c478093db42b03b9cd1d4d6
Alex Oliveira <allex.c.o@hotmail.com>
2024-09-14 20:28:58 -0300

    feat: add user model

--- lib/data/models/models.dart
+++ lib/data/models/models.dart
@@ -1,1 +1,2 @@
 export 'category_model.dart';
+export 'user_model.dart';
---
+++ lib/data/models/user_model.dart
@@ -0,0 +1,42 @@
+import '../../domain/entities/entities.dart';
+import '../../domain/helpers/helpers.dart';
+
+class UserModel {
+  final String id;
+  final String imageUrl;
+  final String name;
+
+  UserModel({
+    required this.id,
+    required this.imageUrl,
+    required this.name,
+  });
+
+  factory UserModel.fromEntity(UserEntity user) {
+    return UserModel(
+      id: user.id,
+      imageUrl: user.imageUrl,
+      name: user.name,
+    );
+  }
+
+  factory UserModel.fromJson(Map json) {
+    try {
+      return UserModel(
+        id: json['id'],
+        imageUrl: json['imageUrl'],
+        name: json['name'],
+      );
+    } catch (_) {
+      throw DomainError.unexpected;
+    }
+  }
+
+  UserEntity toEntity() {
+    return UserEntity(
+      id: id,
+      imageUrl: imageUrl,
+      name: name,
+    );
+  }
+}
