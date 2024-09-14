d11cf579411e65dc32ae1122499b563a82b05747
Alex Oliveira <allex.c.o@hotmail.com>
2024-09-14 19:48:50 -0300

    feat: add star icon widget

---
+++ lib/ui/widgets/star_icon.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StarIcon extends StatelessWidget {
+  const StarIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Icon(Icons.star, color: Color(0xFFFFB800), size: 12);
+  }
+}
