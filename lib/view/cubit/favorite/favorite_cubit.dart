c4200ba251301e3836bf191dc219aba2610e877d
Guilherme Fonseca <43682827+fonsecguilherme@users.noreply.github.com>
2024-09-15 08:36:50 -0300

    fix: cubit validation update

--- lib/view/cubit/favorite/favorite_cubit.dart
+++ lib/view/cubit/favorite/favorite_cubit.dart
@@ -8,68 +8,39 @@ class FavoriteCubit extends Cubit<FavoriteState> {
 
   Future<void> favoriteRestaurant(Restaurant restaurant) async {
     final newFavorites = List<Restaurant>.from(state.favorites);
-    final isAlreadyFavorited = !state.favorites.any(
-      (element) => element.id == restaurant.id,
-    );
-    if (restaurant.id != null) {
-      if (isAlreadyFavorited) {
-        newFavorites.add(restaurant);
-        emit(state.copyWith(status: FavoriteStatus.favoriteSuccess));
+
+    final containsAddress = state.favorites.contains(restaurant);
+
+    if (containsAddress) {
+      newFavorites.remove(restaurant);
+
+      emit(
+        state.copyWith(
+          favorites: newFavorites,
+          status: FavoriteStatus.removed,
+        ),
+      );
+
+      if (newFavorites.isEmpty) {
         emit(
           state.copyWith(
             favorites: newFavorites,
-            status: FavoriteStatus.success,
+            status: FavoriteStatus.initial,
           ),
         );
-      } else {
-        newFavorites.remove(restaurant);
-
-        if (newFavorites.isNotEmpty) {
-          emit(
-            state.copyWith(
-              favorites: newFavorites,
-              status: FavoriteStatus.removed,
-            ),
-          );
-
-          emit(
-            state.copyWith(
-              favorites: newFavorites,
-              status: FavoriteStatus.success,
-            ),
-          );
-        } else {
-          emit(
-            state.copyWith(
-              favorites: newFavorites,
-              status: FavoriteStatus.removed,
-            ),
-          );
-          emit(state.copyWith(status: FavoriteStatus.initial));
-        }
       }
     } else {
+      newFavorites.add(restaurant);
+
       emit(
         state.copyWith(
-          status: FavoriteStatus.failure,
-          errorMessage:
-              'Could favorite this restaurant! Refresh the app to try again! ',
+          status: FavoriteStatus.favoriteSuccess,
         ),
       );
-    }
-  }
-
-  Future<void> loadFavorites() async {
-    emit(state.copyWith(status: FavoriteStatus.loading));
-    if (state.favorites.isEmpty) {
-      emit(
-        state.copyWith(status: FavoriteStatus.initial),
-      );
-    } else {
       emit(
         state.copyWith(
+          favorites: newFavorites,
           status: FavoriteStatus.success,
-          favorites: state.favorites,
         ),
       );
     }
