ac24dcf722a5fe4db1bcaf701f543dbb553e2191
Paolo Joaquin Pinto <paolojoaquinpintoperez@gmail.com>
2024-09-16 09:59:02 -0400

    feat: added loading indicator in data fetch home screen

--- lib/features/home_page/presenter/page/widgets/tab_views.dart
+++ lib/features/home_page/presenter/page/widgets/tab_views.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_tour/core/models/restaurant.dart';
 import 'package:restaurant_tour/features/home_page/presenter/page/widgets/all_restaurants_tab.dart';
 import 'package:restaurant_tour/features/home_page/presenter/page/widgets/my_favorites_tab.dart';
 
-
 class TabViews extends StatelessWidget {
-  const TabViews({super.key});
+  const TabViews({
+    super.key,
+    required this.restaurantList,
+  });
+
+  final List<Restaurant> restaurantList;
 
   @override
   Widget build(BuildContext context) {
