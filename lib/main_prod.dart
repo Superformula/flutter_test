6f4958812ae853ada5670fffe66f7f884ca2dd6b
Guilherme Fonseca <43682827+fonsecguilherme@users.noreply.github.com>
2024-09-16 00:55:23 -0300

    feat: flavors implementation

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant_tour/app.dart';
+import 'package:restaurant_tour/core/flavors.dart';
 
 import 'core/dependency_injection/service_locator.dart';
 
 void main() {
-  setupLocator();
+  WidgetsFlutterBinding.ensureInitialized();
+  setupLocator(flavor: Flavor.prod);
   runApp(const App());
 }
