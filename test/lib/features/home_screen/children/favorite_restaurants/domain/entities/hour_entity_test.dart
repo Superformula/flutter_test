5b81c0fc50e5a4cd74d46a52d01abe8a8099288e
Paolo Joaquin Pinto <paolojoaquinpintoperez@gmail.com>
2024-09-16 15:28:11 -0400

    feat: added unit testing for all restaurants and favorite restaurants

---
+++ test/lib/features/home_screen/children/favorite_restaurants/domain/entities/hour_entity_test.dart
@@ -0,0 +1,43 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:restaurant_tour/features/home_screen/presenter/children/favorite_restaurants/domain/entities/hour_entity.dart';
+
+
+void main() {
+  group(
+    'HourEntity',
+        () {
+      test('should have isOpenNow property correctly assigned', () {
+        const isOpenNow = true;
+        const hourEntity = HourEntity(isOpenNow: isOpenNow);
+
+        expect(hourEntity.isOpenNow, isOpenNow);
+      });
+
+      test('should support value equality based on isOpenNow', () {
+        const hourEntity1 = HourEntity(isOpenNow: true);
+        const hourEntity2 = HourEntity(isOpenNow: true);
+
+        expect(
+          hourEntity1,
+          equals(
+            hourEntity2,
+          ),
+        );
+      });
+
+      test('should not be equal when isOpenNow values differ', () {
+        const hourEntity1 = HourEntity(isOpenNow: true);
+        const hourEntity2 = HourEntity(isOpenNow: false);
+
+        expect(
+          hourEntity1,
+          isNot(
+            equals(
+              hourEntity2,
+            ),
+          ),
+        );
+      });
+    },
+  );
+}
