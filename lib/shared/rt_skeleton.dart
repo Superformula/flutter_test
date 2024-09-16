6319fc0759593709239297a56506743f9ba81695
Paolo Joaquin Pinto <paolojoaquinpintoperez@gmail.com>
2024-09-16 02:42:47 -0400

    feat: layer creation for loading effect with shimmer

---
+++ lib/shared/rt_skeleton.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class RtSkeleton extends StatelessWidget {
+  const RtSkeleton({
+    super.key,
+    required this.marginBottom,
+    required this.borderRadius,
+    required this.width,
+    required this.height,
+  });
+
+  final double marginBottom;
+  final double borderRadius;
+  final double width;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: marginBottom),
+      child: Shimmer.fromColors(
+        baseColor: Colors.grey.shade300,
+        highlightColor: Colors.grey.shade100,
+        child: Container(
+          width: width,
+          height: height,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(borderRadius),
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
