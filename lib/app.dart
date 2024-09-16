ce6b3445a1ea41c6ca7a41abf587ca16d480d643
Guilherme Fonseca <43682827+fonsecguilherme@users.noreply.github.com>
2024-09-16 00:57:04 -0300

    fix: changed import path

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:restaurant_tour/core/dependency_injection/service_locator.dart';
+import 'package:restaurant_tour/data/repositories/yelp_repository.dart';
 import 'package:restaurant_tour/view/cubit/favorite/favorite.dart';
 import 'package:restaurant_tour/view/pages/home/home_page.dart';
 
-import 'data/repositories/yelp_repository.dart';
 import 'view/cubit/restaurants/restaurants.dart';
 
 class App extends StatelessWidget {
