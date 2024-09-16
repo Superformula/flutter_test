20737488b15ed65afe0f3453b07860e704320a0d
Paolo Joaquin Pinto <paolojoaquinpintoperez@gmail.com>
2024-09-16 10:49:29 -0400

    feat: added card restaurant, widget stars rate, status widget

--- lib/features/home_page/presenter/page/widgets/card_restaurant.dart
+++ lib/features/home_page/presenter/page/widgets/card_restaurant.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant_tour/core/models/restaurant.dart';
+import 'package:restaurant_tour/features/home_page/presenter/page/widgets/rate_stars.dart';
+import 'package:restaurant_tour/features/home_page/presenter/page/widgets/status_indicator.dart';
 
 
 class CardRestaurant extends StatelessWidget {
@@ -12,9 +14,92 @@ class CardRestaurant extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        title: Text(restaurant.name ?? 'No name provided'),
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 4.0,
+        horizontal: 8,
+      ),
+      child: Card(
+        child: IntrinsicHeight(
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 90,
+                  height: 90,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(5),
+                    child: Image.network(
+                      restaurant.photos!.first,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        restaurant.name ?? 'No name provided',
+                        maxLines: 2,
+                        style: Theme.of(context).textTheme.titleMedium,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 2.0),
+                        child: Row(
+                          children: [
+                            Text(
+                              restaurant.price ?? '',
+                              style: Theme.of(context).textTheme.bodyMedium,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            Padding(
+                              padding:
+                              const EdgeInsets.symmetric(horizontal: 4.0),
+                              child: Text(
+                                restaurant.categories?.first.title ?? '',
+                                style: Theme.of(context).textTheme.bodyMedium,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Spacer(),
+                      Row(
+                        children: [
+                          RateStars(
+                            rate: restaurant.rating ?? 0.0,
+                            starSize: 20.0,
+                            color: Colors.amber,
+                          ),
+                          const Spacer(),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: restaurant.isOpen
+                                ? const StatusIndicator(
+                              text: "Open Now",
+                              color: Colors.green,
+                            )
+                                : const StatusIndicator(
+                              text: "Closed",
+                              color: Colors.red,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
