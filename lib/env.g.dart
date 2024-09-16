ec8776bef8f2af028d98c99601a078e02f89978e
Helder <helderhsr@msn.com>
2024-09-16 15:02:38 -0300

    env for datasource

---
+++ lib/env.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+class _Env {
+  static const apiKey =
+      'yT1sJ3lRDxaU8jTfgmRuGnvd0Pj-OAU9KqV8VmQ9rK0L-M8sID9ZN0UmBfmPiMTFTHO11ziGqcb6gILaUj7zdkYUfZoUHshf7HsBUd2LEU5nIASGbkfYLaaUPZ_kZnYx';
+  static const baseUrl = 'https://api.yelp.com/v3/graphql';
+}
