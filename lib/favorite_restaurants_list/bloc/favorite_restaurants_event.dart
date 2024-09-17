d2582d41d67fb9574688c2c66356d5502b64d8f7
Enzo Lizama <lizama.enzo@gmail.com>
2024-09-17 17:30:31 -0500

    feat: list favorite restaurants

---
+++ lib/favorite_restaurants_list/bloc/favorite_restaurants_event.dart
@@ -0,0 +1,10 @@
+part of 'favorite_restaurants_bloc.dart';
+
+sealed class FavoriteRestaurantsEvent extends Equatable {
+  const FavoriteRestaurantsEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class FetchFavoriteRestaurants extends FavoriteRestaurantsEvent {}
---
+++ lib/favorite_restaurants_list/favorite_restaurant_list.dart
@@ -0,0 +1,2 @@
+export 'bloc/favorite_restaurants_bloc.dart';
+export 'view/view.dart';
