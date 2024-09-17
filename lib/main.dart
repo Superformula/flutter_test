2afb83f4fc535a227a33682e766f9494d44fa750
Enzo Lizama <lizama.enzo@gmail.com>
2024-09-17 17:30:57 -0500

    chore: update entry point for app

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,20 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart';
 import 'package:restaurant_gql_client/restaurant_gql_client.dart';
 import 'package:restaurant_repository/restaurant_repository.dart';
+import 'package:restaurant_tour/favorite_restaurants_list/bloc/favorite_restaurants_bloc.dart';
+import 'package:restaurant_tour/favorite_restaurants_list/view/view.dart';
 import 'package:restaurant_tour/restaurant_list/restaurant_list.dart';
+import 'package:restaurant_ui/restaurant_ui.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final client = Client();
-  final gqlClient = RestaurantGqlClient(client);
-  final repository = RestaurantRepository(gqlClient);
+  const apiKey = 'apiKey';
+  const baseUrl = 'baseUr';
+  final gqlClient = RestaurantGqlClient(client, baseUrl, apiKey);
+  final sharedPreferences = await SharedPreferences.getInstance();
+  final repository = RestaurantRepository(gqlClient, sharedPreferences);
 
   runApp(RestaurantTour(restaurantRepository: repository));
 }
@@ -23,10 +30,18 @@ class RestaurantTour extends StatelessWidget {
   Widget build(BuildContext context) {
     return RepositoryProvider.value(
       value: restaurantRepository,
-      child: BlocProvider(
-        create: (context) => RestaurantListBloc(
-          context.read<RestaurantRepository>(),
-        )..add(const FetchRestaurantList()),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => RestaurantListBloc(
+              restaurantRepository,
+            )..add(const FetchRestaurantList()),
+          ),
+          BlocProvider(
+            create: (context) => FavoriteRestaurantsBloc(restaurantRepository)
+              ..add(FetchFavoriteRestaurants()),
+          ),
+        ],
         child: const MaterialApp(
           title: 'Restaurant Tour',
           home: HomePage(),
@@ -45,18 +60,28 @@ class HomePage extends StatelessWidget {
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('Restaurant Tour'),
+          centerTitle: true,
+          title: const Text(
+            'Restaurant Tour',
+            style: AppTextStyles.loraRegularHeadline,
+          ),
           bottom: const TabBar(
             tabs: [
-              Text('All Restaurants'),
-              Text('My Favorites'),
+              Text(
+                'All Restaurants',
+                style: AppTextStyles.loraRegularTitle,
+              ),
+              Text(
+                'My Favorites',
+                style: AppTextStyles.loraRegularTitle,
+              ),
             ],
           ),
         ),
-        body: TabBarView(
+        body: const TabBarView(
           children: [
-            const RestaurantListView(),
-            Container(),
+            RestaurantListView(),
+            FavoriteRestaurantsListView(),
           ],
         ),
       ),
